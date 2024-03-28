6367c5dba2363d485c9361000b0c7eeb36f5555b
Danyal Moazzam <danyalmoazzamjan@gmail.com>
2024-03-28 18:24:58 +0500

    Simple CLI Calculator

---
+++ index.ts
@@ -0,0 +1,42 @@
+import inquirer from 'inquirer'
+
+// Asking Question form User through Inquirer
+
+let answers = await inquirer.prompt([
+    {
+        message: "Enter First Number",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message: "Enter Second Number",
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        message: "Select One Operator to Performs Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+
+// Conditional Statements If-Else
+
+if(answers.operator === 'Addition'){
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if(answers.operator === 'Subtraction'){
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if(answers.operator === 'Multiplication'){
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if(answers.operator === 'Division'){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+
+else{
+    console.log("Invalid Input");
+}
